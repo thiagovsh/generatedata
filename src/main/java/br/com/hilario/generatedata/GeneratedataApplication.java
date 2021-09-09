afe2fa20c38b892c13c090497b9d4f0b92592870
Thiago Vieira de Souza Hilario <thhilario@uolinc.com>
2021-09-09 19:03:34 -0300

    primeiro commit

---
+++ src/main/java/br/com/hilario/generatedata/GeneratedataApplication.java
@@ -0,0 +1,13 @@
+package br.com.hilario.generatedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratedataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hilario/generatedata/GeneratedataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hilario.generatedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
